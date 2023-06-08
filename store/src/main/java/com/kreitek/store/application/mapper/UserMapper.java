f34d7bddffd1eb58adc56b442c79553d3d6fb9b3
Haitian_Ji <haitianji123@gmail.com>
2023-06-08 10:35:39 +0200

    Hito 3: 3.0.1

--- store/src/main/java/com/kreitek/store/application/mapper/UserMapper.java
+++ store/src/main/java/com/kreitek/store/application/mapper/UserMapper.java
@@ -6,7 +6,15 @@ import com.kreitek.store.domain.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring",uses={ItemMapper.class,CategoryMapper.class})
 public interface UserMapper extends EntityMapper<UserDTO, User> {
+    @Override
+    @Mapping(target = "carritoItems",ignore = true)
+    User toEntity(UserDTO user);
 
+    @Override
+    @Mapping(target = "carritoItems",ignore = true)
+    List<User> toEntity(List<UserDTO> user);
 }
